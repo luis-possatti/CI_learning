8509ce55bf7532fa8073d7d854ba2a8d0d0e6d71
Luis Henrique D Pozzatti <luis.pozzatti@eldorado.org.br>
2021-05-26 11:45:49 -0300

    fix linter issues

--- test_calculator.py
+++ test_calculator.py
@@ -14,7 +14,7 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-        assert 100 == calculator.multiply(10,10)
+        assert 100 == calculator.multiply(10, 10)
 
     def test_division(self):
-        assert 12.5 == calculator.divide(100,8)
+        assert 12.5 == calculator.divide(100, 8)
