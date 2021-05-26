17338bf98805a43185591824ec04bbe71395383c
Luis Henrique D Pozzatti <luis.pozzatti@eldorado.org.br>
2021-05-26 13:15:11 -0300

    for break @ linter

--- calculator.py
+++ calculator.py
@@ -10,10 +10,8 @@ def add(first_term, second_term):
 def subtract(first_term, second_term):
     return first_term - second_term
 
-
 def multiply(first_term, second_term):
     return first_term*second_term
-
 
 def divide(first_term, second_term) -> float:
     result: float
